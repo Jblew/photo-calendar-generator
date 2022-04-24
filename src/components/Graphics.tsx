7b02aeedf67c2e82d2f5afb87b075cb838d26d70
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-04-24 19:56:01 +0200

    refactor: more informative graphics config names

--- src/components/Graphics.tsx
+++ src/components/Graphics.tsx
@@ -2,14 +2,16 @@ import { getRandomPhotoURL } from "@/services"
 
 export function Graphics() {
     const types = [
-        { className: "mono", count: 1 },
-        { className: "duo", count: 2 },
-        { className: "triptic", count: 3 },
-        { className: "quattro", count: 4 },
-        { className: "quattro-asym", count: 4 },
+        { className: "single", count: 1 },
+        { className: "double", count: 2 },
+        { className: "triple", count: 3 },
+        { className: "four", count: 4 },
+        { className: "four-asym", count: 4 },
         { className: "six", count: 6 },
         { className: "ltb", count: 3 },
         { className: "tbr", count: 3 },
+        { className: "l-five", count: 5 },
+        { className: "r-five", count: 5 },
     ]
     // unseeded random can be used because we generate the html statically
     const type = types[Math.floor(Math.random() * types.length)]
@@ -20,5 +22,5 @@ export function Graphics() {
 
 function Photo() {
     const url = getRandomPhotoURL()
-    return <div style={{ backgroundImage: `url(${url})` }}></div>
+    return <div style={{ backgroundImage: `url("${url}")` }}></div>
 }
--- src/components/MonthPhoto.tsx
+++
@@ -1,5 +0,0 @@
-export function MonthPhoto() {
-    return <figure>
-        <img src="/photo2.jpg" />
-    </figure>
-}
