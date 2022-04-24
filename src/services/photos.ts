bb7c30b6d35712c1bc2cd6c315dc601d12e02cca
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-04-24 19:56:12 +0200

    feat: photo filename filter

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -2,6 +2,8 @@ import { config } from "@/config";
 import * as fs from "fs";
 
 const photoFilenames = fs.readdirSync(config.photosDir)
+    .filter(filename => !filename.startsWith("."))
+
 export function getRandomPhotoURL() {
     const filename = photoFilenames[Math.floor(Math.random() * photoFilenames.length)]
     return `${config.photosURL}/${filename}`;
